3ba69d83fcb6ba632c048fd48caf75a9288bdaba
Leo <contato.leandrovmoreira@gmail.com>
2025-02-10 20:05:18 -0300

    Criação de Classes- escopo

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-def
+class Livros:
+    def __init__(self,titulo,autor,ano):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.livros = []
+
+
+
+    def mostrar_livros(self): 
+        pass
+        print(f"{self.livros}")
+
+
+
+
+
+
+    def cadastrar_livros (self): 
+        livro = {self.titulo ,self.autor,self.ano}        
+        self.livros.append(livro)
+
+
+
+
+
+Teste = Livros("Turma da Monica", "Mauricio de Souza", 1990)
+
+Teste.cadastrar_livros()
+Teste.mostrar_livros()
+
+
+
