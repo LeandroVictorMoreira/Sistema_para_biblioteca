b1d947735712ca3560000d6adbf3f6d6978ebc70
Leo <contato.leandrovmoreira@gmail.com>
2025-02-10 22:06:45 -0300

    Manutenção na classe Leitor

--- main.py
+++ main.py
@@ -1,53 +1,71 @@
-class Livros:
+class Livros: #Classe Livros 
     def __init__(self):
         self.livros = []
 
     def mostrar_livros(self): 
         print(f"{self.livros}")
 
-    def cadastrar_livros (self,titulo,autor,ano): 
-        livro = {'titulo':titulo ,'autor':autor,'ano':ano , 'status': "Disponivel"}        
-        self.livros.append(livro)
+    def cadastrar_livros (self,titulo,autor,ano):  #Função para cadastrar os livros na lista "livros=[]"
+        livro = {'titulo':titulo ,'autor':autor,'ano':ano , 'status': "disponivel"}        
+        self.livros.append(livro) #Append faz a adição do livro na lista
 
 
     def verificador_de_estoque(self,titulo):
 
-        titulo = titulo.strip().lower()
+        titulo = titulo.strip().lower() #Retira os espaços e deixa em minusculo
+        for livro in self.livros:       #Procura o titulo mencionado na lista livros
+            if livro['titulo'].strip().lower()== titulo: #Comparação
+                if livro['status'].strip().lower() == "disponivel":
+                    print(f"O Livro {livro['titulo']} está {livro['status']}")
+                    return True
+
+        print("Esse livro não está disponível em nosso estoque no momento!") 
+
+ 
+
+    def emprestar_livros (self,titulo,nome,leitor):
+
+        cadastro_leitor = leitor.verifica_cadastro(nome)
+        retorno = self.verificador_de_estoque(titulo)
+
         for livro in self.livros:
-            if livro['titulo'].strip().lower()== titulo:
-                if livro['status'].strip().lower() == "Disponivel":
-                    print(f"O Livros {}")
+            if retorno == True and cadastro_leitor == True:
+                print(f"O Livro {livro['titulo']} está sendo emprestado para {nome}.")
+                livro['status'] = 'emprestado'
                 return
-        print("Não Tem") 
-
-    def emprestar_livros (self):
-
-
-        print(f"O Leitor solicitou o emprestimo do livro...{self.livros}")
+            
+        print("Infelizmente esse titulo não está disponivel para emprestimo")
 
 
 class Leitor:
-    def __init__(self,nome):
-        self.nome = nome
-        self.cadastrados = []
+    def __init__(self):
+        self.leitores_cadastrados = []
 
 
-    def cadastrar_leitor(self):
-        leitor = {self.nome}
-        self.cadastrados.append(leitor)
+    def cadastrar_leitor(self,nome):
+        leitor = {'nome':nome}
+        self.leitores_cadastrados.append(leitor)
 
     def mostrar_leitores_cadastrados(self): 
           print(f"{self.cadastrados}")
 
-
+    def verifica_cadastro(self,nome):
+        nome = nome.strip().lower() #Retira os espaços e deixa em minusculo
+        for leitor in self.leitores_cadastrados:
+            if leitor['nome'].strip().lower()== nome.lower():
+                print("Leitor cadastrado!")
+                return True
+        return False
 
 
 
 #Parte da execução
 
-livros = Livros()
+livros =Livros()
+leitor =Leitor()
+leitor.cadastrar_leitor("PedrinhoBH")
 livros.cadastrar_livros("Turma da Monica", "Mauricio de Souza", 1990)
-livros.verificador_de_estoque("Turma da Monica")
+livros.emprestar_livros("Turma da Monica","PedrinhoBH",leitor)
 
 
 #Pessoa = Leitor("Pedrinho BH")
