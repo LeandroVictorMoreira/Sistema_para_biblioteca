bc19f5e5f1700fc98c8382b0052ff5c7bf033fde
Leo <contato.leandrovmoreira@gmail.com>
2025-02-22 21:06:15 -0300

    Adiçao de comentarios, MySQL

--- biblioteca/test/main.py
+++ biblioteca/test/main.py
@@ -1,16 +1,16 @@
-import mysql.connector
+import mysql.connector #Fazer a importação do conector do MySql
 
-meudb = mysql.connector.connect(
+meudb = mysql.connector.connect( 
 host = 'localhost',
 user = 'root',
 password = 'pass09',
 database =  'sistema_para_biblioteca'
 
-)
+)# Uma varivavel que chama o MySql
 
 cursor = meudb.cursor()
 
-cursor.execute("SELECT * FROM alunos")
+cursor.execute("SELECT * FROM alunos") #Selector de Tabela
 
 meuresultado =cursor.fetchall()
 
