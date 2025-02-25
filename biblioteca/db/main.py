996ae070be35b961173abfe10d3eb483ef0ece52
Leo <contato.leandrovmoreira@gmail.com>
2025-02-24 21:51:54 -0300

    Criação de banco de dados proprio)

--- biblioteca/db/main.py
+++ biblioteca/db/main.py
@@ -1,18 +1,16 @@
 import mysql.connector #Fazer a importação do conector do MySql
 
-meudb = mysql.connector.connect( 
+meudb = mysql.connector.connect(  #conexão ao servidor
 host = 'localhost',
 user = 'root',
 password = 'pass09',
 database =  'sistema_para_biblioteca'
 
-)# Uma varivavel que chama o MySql
+)
+
+
+#Criar novo banco de dados
+
 
 cursor = meudb.cursor()
-
-cursor.execute("SELECT * FROM alunos") #Selector de Tabela
-
-meuresultado =cursor.fetchall()
-
-
-print(meuresultado)
+cursor.execute("")
