48aa76dcf8dd6bacf97aa27b08d944fd0f81c77e
Leo Victor <contato@leandrovmoreira@gmail.com>
2025-03-12 22:42:13 -0300

    Merge, troca de maquina

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ leitor.cadastrar_leitor("PedrinhoBH")
 livros.cadastrar_livros("Turma da Monica", "Mauricio de Souza", 1990)
 
 #Realizando emprestimo de Livros, informando titulo, nome do leitor e chamando instancias
-emprestimo.emprestar_livros("Turma da Monica","PedrinhoBH",leitor,livros)
+emprestimo.emprestar_livros("Turma da Monica","PedrinhoBH",leitor,livros)#
 
 
 
