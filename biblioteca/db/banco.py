de6714a264ecf43e77e3abdc99d7dfb5d7dd2c5d
Leo Victor <contato.leandrovmoreira@gmail.com>
2025-03-20 21:55:00 -0300

    Teste de integração com banco de dados

--- biblioteca/db/banco.py
+++ biblioteca/db/banco.py
@@ -6,3 +6,11 @@ meudb = _mysql_connector.connect (
     passoword ='pass09',
     database = 'biblioteca_system'
 ) #Chamada do banco de dados
+
+cursor = meudb.cursor()
+
+cursor.execute ("Select * from alunos")
+
+on_db = cursor.fetchall()
+
+print(on_db)
